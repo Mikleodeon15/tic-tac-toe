588c61c346e4fa690e4df03fdaa2331336fdfb82
Mikleodeon15 <michaelgrung15@gmail.com>
2020-05-04 17:37:20 -0400

    done

--- src/Board.js
+++ src/Board.js
@@ -4,50 +4,63 @@ import Square from './Square';
 const Board = (props) => {
     const { winner } = props;
 
+    function winnerDecoration(attribute) {
+        switch (winner) {
+            case 'x':
+                switch (attribute) {
+                    case 'color':
+                        return 'green-400';
+                    case 'text':
+                        return 'X has Won!';
+                    default:
+                }
+                break;
+            case 'o':
+                switch (attribute) {
+                    case 'color':
+                        return 'orange-400';
+                    case 'text':
+                        return 'O has Won!';
+                    default:
+                }
+                break;
+            case 'no win':
+                switch (attribute) {
+                    case 'color':
+                        return 'red-400';
+                    case 'text':
+                        return 'No winner :(';
+                    default:
+                }
+                break;
+            default:
+                return 'Welcome!';
+        }
+    }
+
     const styles = {
-        boardContainer: `m-10 border-t-2 border-r-2 border-gray-400 border-opacity-50 mx-auto inline-block shadow-xl transform lg:scale-125 md:scale-100 sm:scale-75 ${setBorderColor()}`,
+        header: 'text-center text-4xl m-20 text-gray-800',
+        boardContainer: `m-10 border-t-2 border-r-2 border-gray-400 border-opacity-50 mx-auto inline-block shadow-xl transform lg:scale-125 md:scale-100 sm:scale-75 border-${winnerDecoration(
+            'color'
+        )}`,
         board: 'grid grid-cols-3 grid-row-6',
     };
 
     let squareArray = [];
     for (let i = 0; i < 9; i++) {
         squareArray.push(
-            <Square key={i} {...props} borderColor={setBorderColor} index={i} />
+            <Square
+                key={i}
+                {...props}
+                winnerDecoration={winnerDecoration}
+                index={i}
+            />
         );
-    }
-
-    function setHeader() {
-        switch (winner) {
-            case 'x':
-                return 'X has Won!';
-            case 'o':
-                return 'O has Won!';
-            case 'no win':
-                return 'No winner :(';
-            default:
-                return 'Welcome!';
-        }
-    }
-
-    //Change border color based on winner(if any)
-    function setBorderColor() {
-        switch (winner) {
-            case 'x':
-                return 'border-green-400';
-            case 'o':
-                return 'border-orange-600';
-            case 'no win':
-                return 'border-red-400';
-            default:
-                return '';
-        }
     }
 
     return (
         <div>
-            <h1 className="text-center text-4xl m-20 text-gray-800">
-                {setHeader()}
-            </h1>
+            <h1 className={styles['header']}>{winnerDecoration('text')}</h1>
             <div className={styles['boardContainer']}>
                 <div className={styles['board']}>{squareArray}</div>
             </div>
--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
-const Square = ({ updateBoard, index, round, winner, borderColor }) => {
+const Square = ({ updateBoard, index, round, winner, winnerDecoration }) => {
     const [state, setState] = useState('');
 
     const styles = {
-        square: `w-40 h-40 border-b-2 border-l-2 border-gray-400 border-opacity-50 bg-white m-0 p-0 flex items-center justify-center ${borderColor()}`,
+        square: `w-40 h-40 border-b-2 border-l-2 border-gray-400 border-opacity-50 bg-white m-0 p-0 flex items-center justify-center border-${winnerDecoration(
+            'color'
+        )}`,
         innerText: 'text-6xl text-gray-700',
     };
 
